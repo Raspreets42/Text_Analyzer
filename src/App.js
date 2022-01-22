523e6ec22b3b076d2f5642046c5d185d3794b16a
Raspreets42 <raspreets89@gmail.com>
2022-01-22 17:54:14 +0530

    second Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import './App.css';
+import Navbar from './Components/Navbar';
 import Texting from './Components/Texting';
 
 function App() {
+  const [mode, setMode] = useState("dark");
+
+  const toggleMode = () => {
+    if (mode === "dark") {
+      document.body.style.backgroundColor = "black";
+      document.body.style.color = "white";
+      setMode("light");
+    }
+    else{
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      setMode("dark");
+    }
+  }
+  
   return (
     <>
-      <Texting />
+      <Navbar mode = {mode} toggleMode={toggleMode}/>
+      <Texting mode = {mode} />
     </>
   );
 }
--- src/Components/Texting.js
+++ src/Components/Texting.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-const Texting = () => {
+const Texting = (props) => {
 
     const [text, setTest] = useState("");
     function UpCase() {
@@ -19,15 +19,6 @@ const Texting = () => {
         setTest(event.target.value);
     }
 
-    let lightstyle = {
-        color: "black",
-        backgroundColor: "white"
-    }
-    let darkstyle = {
-        color: "white",
-        backgroundColor: "black"
-    }
-    const [myStyle, setMyStyle] = useState(lightstyle);
     
     let yellowSpaning = {
         color: "yellow",
@@ -39,28 +30,24 @@ const Texting = () => {
         fontWeight: "bold",
         fontStyle: "italic"
     }
-    const [mySpan, setMySpan] = useState(blueSpaning);
-
-    const [myContent , setMyContent] = useState("Enable Dark Theme");
-
-    function darkTheme() {
-        if (myStyle.backgroundColor == "white") {
-            setMyStyle(darkstyle);
-            setMySpan(yellowSpaning);
-            setMyContent("Enable Light Theme");
-        } else {
-            setMyStyle(lightstyle);
-            setMySpan(blueSpaning);
-            setMyContent("Enable Dark Theme");
-        }
-    }
     
+    const copyText = () => {
+        console.log("Copied !!!");
+        var ctext = document.getElementById("textarea");
+        ctext.select();
+        navigator.clipboard.writeText(ctext.value);
+    }
+
+    const removeExtraSpace = () => {
+        console.log("Removed Extra Spaces");
+        let newText = text.split(/[  ]+/);
+        setTest(newText.join(" "));
+    }
     
     return (
         <>
-            <div className="container-fluid py-2" style={myStyle} >
+            <div className="container-fluid py-2"  >
                 <h2 className="text-center my-4">  Text Analyzer  </h2>
-                <button type="button" className="btn btn-outline-primary mx-3" onClick={darkTheme} >{myContent}</button>
                 <div className="container my-4">
                     <hr />
                     <h4 className="my-3">Free software utility which allows you to find the most frequent phrases and frequencies of words. It also counts number of words, characters, sentences and syllables.</h4>
@@ -69,18 +56,20 @@ const Texting = () => {
 
                     <h4 className="text-center my-3">Enter Text ( Copy and Paste is Fine ) Here : </h4>
 
-                    <textarea className="form-control" rows={10} id="floatingTextarea" value={text} onChange={handleOnChange}></textarea>
+                    <textarea className="form-control" rows={10} id="textarea" value={text} onChange={handleOnChange}></textarea>
                 </div>
                 <div className="container text-center">
                     <button type="button" className="btn btn-outline-primary mx-3" onClick={UpCase}>Upper Case</button>
                     <button type="button" className="btn btn-outline-primary mx-3" onClick={Clearing} >Clear</button>
                     <button type="button" className="btn btn-outline-primary mx-3" onClick={LowCase} >Lower Case</button>
+                    <button type="button" className="btn btn-outline-primary mx-3" onClick={copyText} >Copy Text</button>
+                    <button type="button" className="btn btn-outline-primary mx-3" onClick={removeExtraSpace} >Remove Extra Space</button>
                     <hr />
                 </div>
                 <div className="container my-4">
                     <h4 className="text-center">Text Counter</h4>
-                    <h5>Number of Words :  <span style={mySpan}> {text.split(" ").length} </span>  </h5>
-                    <h5>Nuber of Character : <span style={mySpan}> {text.length} </span> </h5>
+                    <h5>Number of Words :  <span style={props.mode==="dark"?blueSpaning:yellowSpaning}> {text.split(" ").length} </span>  </h5>
+                    <h5>Nuber of Character : <span style={props.mode==="dark"?blueSpaning:yellowSpaning}> {text.length} </span> </h5>
                 </div>
 
                 <div className="container my-4">
